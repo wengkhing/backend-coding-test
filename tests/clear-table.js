51097f992c04b068a25e6443bfac8f8073316e63
Weng Khing <wengkhing@seekasia.com>
2021-03-28 10:46:36 +0800

    fix tests with callback

--- tests/clear-table.js
+++ tests/clear-table.js
@@ -1,11 +1,14 @@
 'use strict';
 
-module.exports = (db) => {
+module.exports = (db, done) => {
     const clearTable = 'DELETE FROM Rides';
     const resetSequence = 'UPDATE sqlite_sequence SET seq = 0 WHERE name="Rides"';
 
-    db.run(clearTable);
-    db.run(resetSequence);
+    db.run(clearTable, () => {
+        db.run(resetSequence, () => {
+            done();
+        });
+    });
 
     return db;
 };
