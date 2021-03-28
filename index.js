5e62bb008b8911d67cfbbf3b9c3f6e059d887f4e
Weng Khing <wengkhing@seekasia.com>
2021-03-28 16:39:12 +0800

    apply linting for index

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 'use strict';
 
-const express = require('express');
-const app = express();
 const port = 8010;
 
-const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
-
 const sqlite3 = require('sqlite3').verbose();
+
 const db = new sqlite3.Database(':memory:');
 
 const buildSchemas = require('./src/schemas');
 
+const appConfig = require('./src/app');
+
 db.serialize(() => {
-    buildSchemas(db);
+  buildSchemas(db);
 
-    const app = require('./src/app')(db);
-
-    app.listen(port, () => console.log(`App started and listening on port ${port}`));
+  const app = appConfig(db);
+  // eslint-disable-next-line no-console
+  app.listen(port, () => console.log(`App started and listening on port ${port}`));
 });
